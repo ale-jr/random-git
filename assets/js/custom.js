1493f1f18e4f966ac686c252e640f1be474b5292
alejr <ale.jr.droid@gmail.com>
2017-02-07 00:39:28 -0200

    Consumindo a API do github de forma satisfatória

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,36 +6,42 @@ var reultados_por_pagina = 30;
   $("#label_erro").hide();
 
   //click do botão buscar
-  $("#bt_buscar").click(function(){
+  $(".bt_buscar").click(function(){
     //se usuário selecionar linguagem
     if($("#select_linguagem")[0].selectedIndex > 0){
       //esconde label de erro
       $("#label_erro").hide();
 
-      //define o total de páginas
-      var paginas = 0;
+      //define o total de páginas como 34, pois o git so exibe mil projetos, 30 or página o que são aproximadamente 34 páginas
+      var paginas = 34;
+      var url_pagina = "https://api.github.com/search/repositories?q=language:"+$("#select_linguagem").val() + "&page="+ Math.floor(Math.random() * paginas);
+      console.log(url_pagina);
       $.get("https://api.github.com/search/repositories?q=language:"+$("#select_linguagem").val(), function(data){
-          //obter página aleatória
-
-          //como o git só exibe os mil primeiros resultados, quando total de repos for maior que 1000, o valor para o calculo do total de páginas será de mil
+          //caso o total de projetos seja menor que mil, executa um get com menos páginas
           if(data.total_count<1000){
             paginas = Math.ceil(data.total_count / reultados_por_pagina);
+            url_pagina = "https://api.github.com/search/repositories?q=language:"+$("#select_linguagem").val() + "&page="+ Math.floor(Math.random() * paginas);
+            $.get("https://api.github.com/search/repositories?q=language:"+$("#select_linguagem").val(), function(data2){
+              data = data2;
+            })
+            //caso haja um erro, exibe na label
+            .fail(function(){
+              $("#label_erro").text("Ocorreu um erro, tente novamente");
+              $("#label_erro").show("slow");
+            });
           }
-          else {
-              paginas = Math.ceil(1000 / reultados_por_pagina);
-          }
-          var url_pagina = "https://api.github.com/search/repositories?q=language:"+$("#select_linguagem").val() + "&page="+ Math.floor(Math.random() * paginas);
-          console.log(url_pagina);
-          $.get(url_pagina, function(data){
-
-          })
-          //caso haja um erro, exibe na label
-          .fail(function(){
-            $("#label_erro").text("Ocorreu um erro, tente novamente");
-            $("#label_erro").show("slow");
-          });
-
-
+          console.log(data.items.length);
+          var i = Math.floor(data.items.length * Math.random());
+          var repositorio = data.items[i];
+          console.log(repositorio);
+          //define dados do container
+          $("#img_repo").attr("src",repositorio.owner.avatar_url);
+          $("#nome_repo").text(repositorio.name);
+          $("#dono_repo").text(repositorio.owner.login);
+          $("#dono_repo").attr("href",repositorio.owner.html_url);
+          $("#sobre_repo").text(repositorio.description);
+          $("#bt_ver_repo").attr("href",repositorio.html_url);
+          $("#container").show("slow");
           console.log(paginas);
       },"json")
       //caso haja um erro, exibe na label o erro
